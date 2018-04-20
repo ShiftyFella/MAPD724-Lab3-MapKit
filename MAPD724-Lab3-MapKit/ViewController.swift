5da1e62f4ea1ae92a342b39ae19006777d630634
ShiftyFella <31674713+ShiftyFella@users.noreply.github.com>
2018-04-20 18:44:58 -0400

    Initial Commit

---
+++ MAPD724-Lab3-MapKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MAPD724-Lab3-MapKit
+//
+//  Created by Viktor Bilyk on 2018-04-20.
+//  Copyright © 2018 Brogrammers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
