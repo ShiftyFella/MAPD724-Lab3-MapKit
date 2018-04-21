ae2f7e0e41d047a2f128e01636a0c78d6bd82e41
ShiftyFella <31674713+ShiftyFella@users.noreply.github.com>
2018-04-20 21:03:51 -0400

    Display marker on map with coords

--- MAPD724-Lab3-MapKit/ViewController.swift
+++ MAPD724-Lab3-MapKit/ViewController.swift
@@ -9,18 +9,21 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     //ACTIONS
     @IBAction func changeMapType(_ sender: UISegmentedControl) {//change map view type
         mapView.mapType = MKMapType.init(rawValue: UInt(sender.selectedSegmentIndex)) ?? .standard
-
     }
     
     @IBAction func addLocation(_ sender: UIBarButtonItem) {
         addCoordsPopUp.isHidden = false
     }
     @IBAction func dismissPopUp(_ sender: UIButton) {
+        
+        latitudeTF.text = ""
+        longitudeTF.text = ""
+        
         addCoordsPopUp.isHidden = true
     }
     @IBAction func addLocationMarker(_ sender: UIButton) {
@@ -28,8 +31,11 @@ class ViewController: UIViewController {
         lat = Double(latitudeTF.text!)!
         long = Double(longitudeTF.text!)!
         
-        addCoordsPopUp.isHidden = true
+        latitudeTF.text = ""
+        longitudeTF.text = ""
         
+        addMarkerOnMap(lat: lat, long: long)
+        addCoordsPopUp.isHidden = true
     }
     
     func centerMap(coords: CLLocation) { //center map on location with radius of 1.2 Km
@@ -37,11 +43,26 @@ class ViewController: UIViewController {
         
         mapView.setRegion(region, animated: true)
     }
+    
+    func addMarkerOnMap(lat: Double, long: Double) {//create marker with select coords
+        let pos = CLLocation(latitude: lat, longitude: long)
+        
+        let marker = MKPointAnnotation()
+        
+        marker.title = "My Location"
+        marker.coordinate = pos.coordinate
+        
+        mapView.addAnnotation(marker)
+        centerMap(coords: pos)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        longitudeTF.delegate = self
+        latitudeTF.delegate = self
         
-        
+        //style-popup
         addCoordsPopUp.isHidden = true
         addCoordsPopUp.layer.cornerRadius = 10
         addCoordsPopUp.layer.masksToBounds = true
@@ -52,6 +73,11 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 
     //OUTLETS
